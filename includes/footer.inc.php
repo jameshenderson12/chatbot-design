4e9137807ec50019f391eaa3f2f255e6787b0385
James Henderson <jameshenderson12@hotmail.com>
2022-03-31 22:53:52 +0100

    Rearranged order of external file calls

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -3,7 +3,3 @@
         <span class="text-light small">&copy; 2021-22 CEPEH Project<span class="m-2">&middot;</span>Educational Chatbot Crowd-based Co-creation Tool [v<?php echo $config['application_version']; ?>]</span>
       </div>
     </footer>
-    <!-- Bootstrap core JavaScript -->
-    <!-- Placed at the end of the document for load performance -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
--- includes/header.inc.php
+++ includes/header.inc.php
@@ -17,3 +17,7 @@
   	<!--<link href="../../assets/dist/css/bootstrap.min.css" rel="stylesheet">-->
     <!--<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Bootstrap core JavaScript -->
+    <!-- Placed at the end of the document for load performance -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
