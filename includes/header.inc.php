e8ffde60c469df48a5cfc1b7632319f0a1639809
James Henderson <jameshenderson12@hotmail.com>
2022-05-06 15:56:37 +0100

    Minor formatting amendment

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-	<title><?php echo $page_title.' - '.$config['application_name'].' - '.$config['project_acronym'].': '.$config['project_name']; ?></title>
+	   <title><?php echo $page_title.' - '.$config['application_name'].' - '.$config['project_acronym'].': '.$config['project_name']; ?></title>
     <!-- Various CSS and Font links for including -->
     <link href="<?php echo $config['base_url']; ?>/css/custom.css" rel="stylesheet" type="text/css">
   	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
